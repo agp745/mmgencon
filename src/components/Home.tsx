3e9993ba997cc14889f75d30de5ea86df7a92c80
Alex Perez <alejandro.gperez745@gmail.com>
2023-06-19 10:58:07 -0500

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 
 function HowItWorks() {
-
+    
     return (
         <>
             <h1>HOW IT WORKS</h1>
@@ -12,11 +12,20 @@ export default function Home() {
 
     return (
         <>
-        <section className="flex flex-col items-center h-screen">
-            <h1 className="mt-56 text-3xl text-center font-thin">MM General Contracting & Handyman Services</h1>
-            <h2>INSURED AND CERTIFED HABE, MBE, & HUB</h2>
-            <div>We help with the upkeep of your most important investment, your home.</div>
-            <div>Small and medium remodels and handyman services</div>
+        <section className="flex flex-col items-center text-center h-screen">
+            <h1 className="mt-48 text-5xl text-center font-semibold">MM General Contracting & Handyman Services</h1>
+            <div className="CERTIFICATIONS border-b-2 border-black">
+                <h2 className="mt-9 text-4xl font-light">INSURED AND CERTIFED</h2>
+                <div className="mt-1 flex justify-center gap-2 text-3xl font-black pb-2">
+                    <div className="">HABE</div>
+                    <div>•</div>
+                    <div className="">MBE</div>
+                    <div>•</div>
+                    <div className="">HUB</div>
+                </div>
+            </div>
+            <div className="mt-10 text-2xl intalic font-thin">Small and medium remodels and handyman services</div>
+            <div className="mt-3 text-xl">We help with the upkeep of your most important investment, <span className="">your home</span></div>
         </section>
         <HowItWorks />
         </>
