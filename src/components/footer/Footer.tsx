ccf8eee6fb9f0469408aa303870bb74597c4bda7
Alex Perez <alexgp@unknown020fb5692481.attlocal.net>
2023-08-11 17:36:12 -0500

    updated footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,9 +2,38 @@
 
 export function Footer() {
     return (
-        <section className="">
-            <div className="w-full h-16 bg-mm-light"></div>
-            <div className="w-full h-12 bg-mm-dark"></div>
-        </section>
+        <footer className="">
+            <section className="w-full h-16 bg-mm-light"></section>
+            <section className="w-full min-h-[7rem] bg-mm-dark flex items-center justify-between px-10">
+                <div className="flex flex-col text-lg text-slate-700">
+                    <p>Houston, TX</p>
+                    <p>(713) 319-7492</p>
+                </div>
+                <div className="flex gap-2">
+                    <a 
+                        href="https://www.linkedin.com/in/michael-morales-4313343a/" 
+                        target="_blank"
+                        className="text-slate-700 underline hover:text-slate-800 transition-colors duration-75 ease-in"
+                    >
+                        LinkedIn
+                    </a>
+                    <a 
+                        href="https://www.yelp.com/biz/mm-general-contracting-and-handyman-services-katy" 
+                        target="_blank"
+                        className="text-slate-700 underline hover:text-slate-800 transition-colors duration-75 ease-in"
+                    >
+                        Yelp
+                    </a>
+                    <a 
+                        href="https://nextdoor.com/pages/mm-general-contracting-houston-tx/" 
+                        target="_blank"
+                        className="text-slate-700 underline hover:text-slate-800 transition-colors duration-75 ease-in"
+                    >
+                        NextDoor
+                    </a>
+                </div>
+            </section>
+        </footer>
     )
 }
+
