09e96614080add1c92ccb13b44050491bf70a0f5
Alex Perez <alexgp@unknown020fb5692481.attlocal.net>
2023-08-24 15:23:44 -0500

    mobile friendly home page

--- src/components/slideshow/slideshow.tsx
+++ src/components/slideshow/slideshow.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
+// import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 
 import type { MainHero } from "../../pages/home/Home"
 
@@ -14,9 +14,9 @@ export function Slideshow({ slides }: SlideshowProps) {
     const nextSlide = () => {
         setCurrentSlide((prevSlide) => (prevSlide < slides.length ? prevSlide + 1 : 1))
     }
-    const prevSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide > 1 ? prevSlide - 1 : slides.length))
-    }
+    // const prevSlide = () => {
+    //     setCurrentSlide((prevSlide) => (prevSlide > 1 ? prevSlide - 1 : slides.length))
+    // }
 
     useEffect(() => {
         const interval = setInterval(nextSlide, 5000)
@@ -31,10 +31,10 @@ export function Slideshow({ slides }: SlideshowProps) {
                 style={{ transform: `translateX(-${(currentSlide - 1) * 100}%)` }}
             >
                 {slides.map((slide, index) => (
-                    <div key={index} className="w-full flex-shrink-0">
+                    <div key={index} className="w-full flex-shrink-0 overflow-hidden text-center">
                         <div className="relative">
                             <img src={slide.photo} alt={`${slide.type} slide`} className="w-full h-[50rem]" />
-                            <div className="absolute top-1/2 right-0 left-0 flex justify-center">
+                            <div className="absolute top-1/3 sm:top-1/2 right-0 left-0 flex justify-center ">
                                 <div className="flex flex-col">
                                     <div className="uppercase text-5xl tracking-[0.5rem] font-light bg-neutral-100/75 py-10 px-40 -mt-10">{slide.type} services</div>
                                     <div className="uppercase text-4xl bg-black text-white text-center py-5 flex flex-col justify-center items-center">
@@ -47,8 +47,7 @@ export function Slideshow({ slides }: SlideshowProps) {
                     </div>
                 ))} 
             </div>
-
-            <div className="absolute top-1/2 left-0 right-0 flex justify-between px-4">
+            {/* <div className="absolute top-1/2 left-0 right-0 flex justify-between px-4">
                 <ChevronLeftIcon 
                     onClick={prevSlide}
                     className="w-10 h-10 text-neutral-200 cursor-pointer hover:brightness-125 transition-all duration-75 ease-in"
@@ -58,7 +57,7 @@ export function Slideshow({ slides }: SlideshowProps) {
                     onClick={nextSlide}
                     className="w-10 h-10 text-neutral-200 cursor-pointer hover:brightness-125 transition-all duration-75 ease-in"
                 />
-            </div>
+            </div> */}
         </div>
         <div className="w-full flex justify-center gap-1 pt-1">
             {slides.map((_, index) => {
