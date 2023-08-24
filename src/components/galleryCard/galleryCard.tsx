db5c5ef53f3f6dd3172384f7d0d0f4680c57f9bb
Alex Perez <alexgp@unknown020fb5692481.attlocal.net>
2023-08-24 16:43:46 -0500

    mobile friendly gallery

--- src/components/galleryCard/galleryCard.tsx
+++ src/components/galleryCard/galleryCard.tsx
@@ -10,17 +10,17 @@ export function GalleryCardLeft({src, alt, title, description, location}: Galler
 
     return (
         <section className="flex">
-            <div className="w-1/2 overflow-hidden">
+            <div className="sm:w-1/2 w-2/3 overflow-hidden">
                 <img
                     src={src}
                     alt={alt ? alt : 'gallery photo'}
                     className=""
                 />
             </div>
-            <div className="flex flex-col items-start w-1/2 bg-sky-700/10 pl-10 text-3xl font-light pt-3">
-                <h1 className="uppercase tracking-widest">{title}</h1>
-                <h2 className="text-xl tracking-widesr">{location ? location : ''}</h2>
-                <p className="text-lg">{description ? description : ''}</p>
+            <div className="flex flex-col items-start sm:w-1/2 w-1/3 bg-sky-700/10 sm:pl-10 pl-2 font-light pt-3 text-left">
+                <h1 className="uppercase tracking-widest sm:text-3xl text-xl">{title}</h1>
+                <h2 className="sm:text-xl text-lg font-extralight tracking-wider">{location ? location : ''}</h2>
+                <p className="text-lg mt-2">{description ? description : ''}</p>
             </div>
         </section>
     )
@@ -30,12 +30,12 @@ export function GalleryCardRight({src, alt, title, description, location}: Galle
 
     return (
         <section className="flex">
-            <div className="flex flex-col items-end w-1/2 bg-slate-700/10 pr-10 text-3xl font-light pt-3">
-                <h1 className="uppercase tracking-widest">{title}</h1>
-                <h2 className="text-xl tracking-widesr">{location ? location : ''}</h2>
+            <div className="flex flex-col items-end sm:gap-0 gap-2 sm:w-1/2 w-1/3 bg-slate-700/10 sm:pr-10 pr-2 font-light pt-3 text-right">
+                <h1 className="uppercase tracking-widest sm:text-3xl text-xl">{title}</h1>
+                <h2 className="sm:text-xl text-lg font-extralight tracking-wider">{location ? location : ''}</h2>
                 <p className="text-lg">{description ? description : ''}</p>
             </div>
-            <div className="w-1/2 overflow-hidden">
+            <div className="sm:w-1/2 w-2/3 overflow-hidden">
                 <img
                     src={src}
                     alt={alt ? alt : 'gallery photo'}
